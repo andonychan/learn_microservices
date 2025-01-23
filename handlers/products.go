d76b31f5265141e33fc787742a9e59e34cb17c09
Andy Chan <andy_chan96@hotmail.com>
2025-01-24 03:40:24 +0800

    Created POST and PUT methods using standard lib

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"intro/product-api/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -16,22 +18,88 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
-	// handle get
+	// handle get (standard lib)
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
 		return
 	}
 
-	// handle update
+	// handle post (standard lib)
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	// handle put (standard lib)
+	if r.Method == http.MethodPut {
+		p.l.Println("Attempt PUT for URI ", r.URL.Path)
+		// expect ID in URI
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		g := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		//p.l.Println(g)
+		if len(g) != 1 {
+			p.l.Println("More than 1 id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("More than 1 capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Unable to convert to number")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+		p.l.Println("got id", id)
+
+		p.updateProduct(rw, r, id)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
 	lp := data.GetProduct()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Error marshalling product", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Error unmarshalling product", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request, id int) {
+	p.l.Println("Handle PUT Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Error unmarshalling product", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Error updating product", http.StatusInternalServerError)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
 	}
 }
--- product-api/data/products.go
+++ product-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -19,14 +20,50 @@ type Product struct {
 
 type Products []*Product
 
-func (p Products) ToJSON(w io.Writer) error {
+func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
 }
 
 func GetProduct() Products {
 	return productList
 }
+
+func AddProduct(p *Product) {
+	p.ID = GetNextID()
+	productList = append(productList, p)
+}
+
+func GetNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, idx, err := FindProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[idx] = p
+	return nil
+}
+
+func FindProduct(id int) (*Product, int, error) {
+	for i, product := range productList {
+		if product.ID == id {
+			return product, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+var ErrProductNotFound = fmt.Errorf("product not found")
 
 var productList = []*Product{
 	&Product{
